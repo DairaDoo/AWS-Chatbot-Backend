b2a05ee0c5f01014f3861fdd98b3f2d61eed25c6
Dairan De Jesús <dairandemora.work@gmail.com>
2025-05-05 14:56:35 -0400

    Versión del código con Swagger

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask, request, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
 from rag_pipeline import query_rag_pipeline
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+### Swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'  # Define la ruta a tu archivo swagger.json
+CORS(app)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "RAG Chatbot API"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+### end Swagger specific ###
 
 @app.route('/consultar', methods=['POST'])
 def consultar():
